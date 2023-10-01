ef34421928362bfc8f76cf507528bd825b54a8d1
Nay31code <145429089+Nay31code@users.noreply.github.com>
2023-10-01 21:32:57 +0700

    Update 0Home.py
    Signed-off-by: Nay31code <145429089+Nay31code@users.noreply.github.com>

--- 0Home.py
+++ 0Home.py
@@ -25,7 +25,7 @@ def set_background(png_file):
     ''' % bin_str
     st.markdown(page_bg_img, unsafe_allow_html=True) 
 
-image_path = 'C:\\Users\\A_R_T\\Desktop\\Projec ed\\water-texture-background-pastel-blue-design.jpg - Shortcut.lnk'
+image_path = 'C:\Users\A_R_T\Desktop\Projec ed\water-texture-background-pastel-blue-design.jpg'
 
 
 
